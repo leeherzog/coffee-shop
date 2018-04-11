e6b2c9b15a23250efb622ecf08e7d34143e98707
Ronit Herzog <lee.herzog5@gmail.com>
2018-04-11 13:15:20 +0300

    solution

--- main.js
+++ main.js
@@ -9,12 +9,27 @@ var coffeeShop = {
   },
 
   makeDrink: function (drinkType) {
-    // TODO: Finish this method
+    if (drinkType in coffeeShop.drinkRequirements === false) {
+      alert("Sorry, we don't make " + drinkType);
+    }
+    else if (drinkType in coffeeShop.drinkRequirements && coffeeShop.beans- coffeeShop.drinkRequirements[drinkType] > 0) {
+      coffeeShop.beans = coffeeShop.beans - coffeeShop.drinkRequirements[drinkType];
+    }
+    else if (drinkType in coffeeShop.drinkRequirements && coffeeShop.beans - coffeeShop.drinkRequirements[drinkType] < 0){
+      alert("Sorry, we're all out of beans!");
+    }
   }
 }
 
+console.log(coffeeShop.beans);
 coffeeShop.makeDrink("latte");
+console.log(coffeeShop.beans);
 coffeeShop.makeDrink("americano");
+console.log(coffeeShop.beans);
 coffeeShop.makeDrink("filtered");
+console.log(coffeeShop.beans);
 coffeeShop.makeDrink("doubleShot");
+console.log(coffeeShop.beans);
 coffeeShop.makeDrink("frenchPress");
+console.log(coffeeShop.beans);
+
